b1c1d2e85529132f3ddb00825a266c23960b2b95
khanhytran <123288181+khanhytran@users.noreply.github.com>
2023-12-09 22:59:12 +0700

    update checkOut page code

---
+++ project/checkOut/checkOut.js
@@ -0,0 +1,28 @@
+let listCart
+let totalPrice = 0
+function render(){ 
+    // lấy dữ liệu từ localStorage về
+    let listCartJson = localStorage.getItem("listCart");
+    listCart = JSON.parse(listCartJson);
+    let content="";
+    for(let i=0; i<listCart.length; i++){
+        let total = Number(listCart[i].sl)*Number(listCart[i].item.price)
+        content += `
+        <tr>
+            <td class="text-left"><img src="${listCart[i].item.img[0]}" alt=""></td>
+            <td class="text-left">
+                <h3>${listCart[i].item.nameProduct}</h3>
+                <p>Số lượng: ${listCart[i].sl}</p>
+            </td>
+            <td class="text-right">${(total.toFixed(3)) + 'VND'}</td>
+        </tr>
+        `;
+        totalPrice += total
+    }    
+    let contentItem=document.querySelector(".cart-row",)
+    contentItem.innerHTML=content
+
+    document.querySelector('.total-price').innerHTML = totalPrice.toFixed(3)
+}
+
+render()
